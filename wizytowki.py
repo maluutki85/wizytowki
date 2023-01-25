2c83d05c450994e88327798e41c02d76e5786288
Jakub Dziurgot <j.dziurgot@heating.pl>
2023-01-25 18:24:21 +0100

    Test version

--- wizytowki.py
+++ wizytowki.py
@@ -10,58 +10,105 @@ class Cards:
         return f"{self.imie} {self.nazwisko} {self.mail}"
 
     def contact(self):
-        return f"Kontaktuję się z {self.imie} {self.nazwisko} {self.mail}"
+        print(
+            f"Kontaktuję się z {self.imie} {self.nazwisko} {self.stanowisko} {self.mail}"
+        )
+
+    @property
+    def sum(self):
+        print(len(self.imie + " " + self.nazwisko))
 
 
-Jakub = Cards(
-    imie="Jakub",
-    nazwisko="Dziurgot",
-    firma="Heating",
-    stanowisko="Logistyk",
-    mail="j.dziurgot@heating.pl",
-)
-Jacek = Cards(
-    imie="Jacek",
-    nazwisko="Nowak",
-    firma="ACV",
-    stanowisko="Kierownik",
-    mail="j.nowak@acv.pl",
-)
-Jan = Cards(
-    imie="Jan",
-    nazwisko="Kowalski",
-    firma="AED",
-    stanowisko="Prezes",
-    mail="j.kowalski@aed.pl",
-)
-Paweł = Cards(
-    imie="Paweł",
-    nazwisko="Adamczyk",
-    firma="RAZ",
-    stanowisko="Sprzedawca",
-    mail="p.adamczyk@raz.pl",
-)
-Andrzej = Cards(
-    imie="Andrzej",
-    nazwisko="Sobota",
-    firma="Heat",
-    stanowisko="Magazynier",
-    mail="a.sobota@heat.pl",
-)
-cards = [Jakub, Jacek, Jan, Paweł, Andrzej]
+class BaseContact(Cards):
+    def __init__(self, imie, nazwisko, telefon, mail):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.telefon = telefon
+        self.mail = mail
+
+    def contact(self):
+        print(f"Wybieram numer {self.telefon} i dzownię do {self.imie}{self.nazwisko}")
+
+    @property
+    def sum(self):
+        print(len(self.imie + " " + self.nazwisko))
 
 
-for obj in cards:
-    print(obj)
-print("---")
+class BusinessContact(Cards):
+    def __init__(self, stanowisko, firma, telefon1):
+        self.stanowisko = stanowisko
+        self.firma = firma
+        self.telefon1 = telefon1
 
-for card in sorted(cards, key=lambda card: card.imie):
-    print(card)
-print("---")
+    def contact(self):
+        print(f"Wybieram numer {self.telefon1} i dzownię do {self.imie}{self.nazwisko}")
 
-for card in sorted(cards, key=lambda card: card.nazwisko):
-    print(card)
-print("---")
+    @property
+    def sum(self):
+        print(len(self.imie + " " + self.nazwisko))
 
-for card in sorted(cards, key=lambda card: card.mail):
-    print(card)
+
+if __name__ == "__main__":
+
+    Jakub = Cards(
+        imie="Jakub",
+        nazwisko="Dziurgot",
+        firma="Heating",
+        stanowisko="Logistyk",
+        mail="j.dziurgot@heating.pl",
+    )
+    Jacek = Cards(
+        imie="Jacek",
+        nazwisko="Nowak",
+        firma="ACV",
+        stanowisko="Kierownik",
+        mail="j.nowak@acv.pl",
+    )
+    Jan = Cards(
+        imie="Jan",
+        nazwisko="Kowalski",
+        firma="AED",
+        stanowisko="Prezes",
+        mail="j.kowalski@aed.pl",
+    )
+    Pawel = Cards(
+        imie="Paweł",
+        nazwisko="Adamczyk",
+        firma="RAZ",
+        stanowisko="Sprzedawca",
+        mail="p.adamczyk@raz.pl",
+    )
+    Andrzej = Cards(
+        imie="Andrzej",
+        nazwisko="Sobota",
+        firma="Heat",
+        stanowisko="Magazynier",
+        mail="a.sobota@heat.pl",
+    )
+    cards = [Jakub, Jacek, Jan, Pawel, Andrzej]
+
+    for obj in cards:
+        print(obj)
+    print("---")
+
+    for card in sorted(cards, key=lambda card: card.imie):
+        print(card)
+    print("---")
+    for card in sorted(cards, key=lambda card: card.nazwisko):
+        print(card)
+    print("---")
+    for card in sorted(cards, key=lambda card: card.mail):
+        print(card)
+    print("---")
+
+    Jakub.contact()
+    Jacek.contact()
+    Jan.contact()
+    Pawel.contact()
+    Andrzej.contact()
+
+    Jakub.sum
+    Jacek.sum
+    Jan.sum
+    Pawel.sum
+    Andrzej.sum
