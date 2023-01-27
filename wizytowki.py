b147cc4c6da024c36b3a7ea8fe073f2384c2c881
maluutki85 <118923779+maluutki85@users.noreply.github.com>
2023-01-27 10:05:41 +0100

    Add create_contacts

--- wizytowki.py
+++ wizytowki.py
@@ -1,3 +1,8 @@
+from faker import Faker
+
+fake = Faker("pl_PL")
+
+
 class Cards:
     def __init__(self, imie, nazwisko, telefon, mail):
         self.imie = imie
@@ -16,6 +21,9 @@ class Cards:
         return f"{self.imie}, {self.nazwisko}, {self.mail}"
 
 
+# klasa BusinessContact
+
+
 class BusinessContact(Cards):
     def __init__(self, firma, stanowisko, telefon1, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -23,9 +31,23 @@ class BusinessContact(Cards):
         self.stanowisko = stanowisko
         self.telefon1 = telefon1
 
-    @property
-    def label_length(self):
-        return len(self.imie + " " + self.nazwisko)
-
     def contact(self):
-        print(f"Wybieram {self.telefon1}" f" i dzwonię do {self.imie} {self.nazwisko}")
+        print(f"Wybieram {self.telefon1} i dzwonię do {self.imie} {self.nazwisko}")
+
+
+# funkcja create_contacts
+
+if __name__ == "__main__":
+
+    rodzaj_wizytowki = int(
+        input("Jaki rodzaj wizytówki chcesz utworzyć? 1 - prywatna, 2 - firmowa: ")
+    )
+    ilosc_wizytowki = int(input("Ile wizytówek chcesz utworzyć?"))
+
+    for i in range(ilosc_wizytowki):
+        if rodzaj_wizytowki == 1:
+            card = f"{fake.first_name()} {fake.last_name()}, {fake.phone_number()}, {fake.email()}"
+            print(card)
+        elif rodzaj_wizytowki == 2:
+            card = f"{fake.first_name()} {fake.last_name()}, {fake.phone_number()}, {fake.email()}, {fake.job()},{fake.company()}"
+            print(card)
