5dee9e9fc9aac4773e1952cfbc19c27edc9eb96a
maluutki85 <118923779+maluutki85@users.noreply.github.com>
2023-02-01 21:07:14 +0100

    Update wizytowki.py

--- wizytowki.py
+++ wizytowki.py
@@ -2,50 +2,49 @@ from faker import Faker
 
 fake = Faker("pl_PL")
 
-
-class Cards:
-    def __init__(self, imie, nazwisko, telefon, mail):
+class BaseContact:
+    def __init__(self,imie,telefon,mail):
         self.imie = imie
-        self.nazwisko = nazwisko
         self.telefon = telefon
         self.mail = mail
 
     @property
-    def label_length(self):
-        return len(self.imie + " " + self.nazwisko)
-
+    def label_lenght(self): 
+        return len(self.imie)
+    
     def contact(self):
-        print(f"Wybieram {self.telefon}" f" i dzwonię do {self.imie} {self.nazwisko}")
+        return f"Wybieram {self.telefon} i dzwonię do {self.imie}"
 
-    def __str__(self):
-        return f"{self.imie}, {self.nazwisko}, {self.mail}"
-
-
-# klasa BusinessContact
+    def __repr__(self):
+        return f'{self.imie}, {self.telefon}, {self.mail}'
 
 
-class BusinessContact(Cards):
-    def __init__(self, firma, stanowisko, telefon1, *args, **kwargs):
+class BusinessContact(BaseContact):
+    def __init__(self, stanowisko, firma, telefon1, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.firma = firma
         self.stanowisko = stanowisko
+        self.firma = firma
         self.telefon1 = telefon1
 
+
     def contact(self):
-        print(f"Wybieram {self.telefon1} i dzwonię do {self.imie} {self.nazwisko}")
+        return f"Wybieram {self.telefon1} i dzwonię do {self.imie}"
+    
+    def __repr__(self):
+        return f'{self.imie}, {self.telefon}, {self.mail}, {self.stanowisko}, {self.firma}, {self.telefon1}'
+
+def create_contacts(rodzaj, ilosc):
+    my_list = []
+    if rodzaj == "prywatna":
+        for i in range (ilosc):
+            my_list.append(BaseContact(imie = fake.name(),telefon = fake.phone_number(),mail=fake.email()))
+    elif rodzaj == "firmowa":
+        for i in range (ilosc):
+            my_list.append(BusinessContact(imie = fake.name(),telefon = fake.phone_number(),mail=fake.email(),stanowisko=fake.job(),firma = fake.company(), telefon1 = fake.phone_number()))
+    print(my_list)
 
 if __name__ == "__main__":
-    
-    def create_contacts(rodzaj, ilosc):
-
-        if rodzaj == "prywatna":
-            for i in range(ilosc):
-                card = f"{fake.first_name()} {fake.last_name()}, {fake.phone_number()}, {fake.email()}"
-                print(card)
-        elif rodzaj == "firmowa":
-            for i in range(ilosc):
-                card = f"{fake.first_name()} {fake.last_name()}, {fake.phone_number()}, {fake.email()}, {fake.job()},{fake.company()}"
-                print(card)
-
-
-create_contacts("firmowa", 10)
+  
+  create_contacts("prywatna",5)
+  print("---")
+  create_contacts("firmowa",5)
